c4771053e909d8138e6178fa98f867de6773656c
Taylor Edmiston <tedmiston@gmail.com>
2017-10-29 15:55:02 -0400

    Sleep after running query

--- puller.py
+++ puller.py
@@ -19,8 +19,6 @@ i = 0
 while True:
 # for i in range(2):
     print(f'run {i}')
-    time.sleep(60)
-    # time.sleep(5)
 
     ts = datetime.datetime.now()
     df = pd.read_sql('select state, count(*) from task_instance group by state;', conn)
@@ -29,6 +27,8 @@ while True:
     all_df = all_df.append(df, ignore_index=True, verify_integrity=True)
     # all_df = all_df.append(df)
 
+    time.sleep(60)
+    # time.sleep(5)
     # del df
 
     all_df.to_json('data.json')
