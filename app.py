908759f6b6e6b573d8f75a4a0b31d284a90eb3eb
Viraj Parekh <viraj@astronomer.io>
2017-10-29 16:10:53 -0400

    Fixing .gitignore stuff

--- app.py
+++ app.py
@@ -1,29 +1,52 @@
 # -*- coding: utf-8 -*-
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash()
+from plotly import graph_objs as go
+from datetime import datetime as dt
+import json
+import pandas as pd
+import os
+from flask import Flask
 
+df = pd.read_json('data.json').sort_values(by = 'timestamp')
+
+data = {}
+for state in df['state'].tolist():
+    trace = go.Scatter(
+            x = df[df['state'] == state]['timestamp'],
+            y = df[df['state'] == state]['count'],
+            name = state,
+            opacity = 0.8
+    )
+    data[state] = trace
+
+app = dash.Dash()
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='clickXTREME'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Ain't this some shit.
     '''),
 
     dcc.Graph(
-        id='example-graph',
+        id='sample_one',
         figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
+            'data':
+                list(data.values()),
+            'layout': go.Layout(
+                hovermode='closest',
+                yaxis=dict(
+                    type='log',
+                    autorange=True
+                    )
+
+            )
+
             }
-        }
-    )
+)
 ])
 
 if __name__ == '__main__':
